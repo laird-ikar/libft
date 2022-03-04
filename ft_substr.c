264a9773210c3d5adae1270a8e298b5d1687fe7a
laird-ikar <bguyot@k1r1p10.42mulhouse.fr>
2022-03-04 08:46:52 +0100

    jvous jure si ca marche pas je (╯°□°）╯︵ ┻━┻

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:24:27 by bguyot            #+#    #+#             */
-/*   Updated: 2022/02/23 13:24:32 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/03/04 08:45:44 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*res;
 
-	res = malloc ((len + 1) * sizeof (char));
-	if (!res || !s)
+	if (!s)
+		return (NULL);
+	res = malloc ((ft_min(len, ft_strlen(s)) + 1) * sizeof (char));
+	if (!res)
 		return (NULL);
 	i = -1;
 	while (++i < len && s[start + i])
